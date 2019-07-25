9eeea9fc26226f9484660f73375040c9a72ed088
Vishal <Vishal@192.168.0.122>
2019-07-25 18:48:23 +0200

    making some changes

--- feedProj/RSS/class_lib.php
+++ feedProj/RSS/class_lib.php
@@ -2,16 +2,16 @@
 
 class posts
 {
-    private  $url;    
-    private $feedobject;   
+    private  $url;
+    private $feedobject;
     
-    /* Constructor to intialize class url value.*/    
+    /* Constructor to intialize class url value.*/
     function __construct($urlVal)
     {
-        $this->url = $urlVal;       
+        $this->url = $urlVal;
     }
     
-    /* This function will return all the items  from the XML .*/    
+    /* This function will return all the items  from the XML .*/
     function getItems()
     {
         try{
@@ -20,8 +20,8 @@ class posts
             $res  = $this->feedobject->load($this->url);
             if($res  == 1)
             {
-                 $contentItem = $this->feedobject->getElementsByTagName('item');
-                 return $contentItem;
+                $contentItem = $this->feedobject->getElementsByTagName('item');
+                return $contentItem;
             }
         }catch(DOMException $Exception){
             return null;
@@ -36,7 +36,7 @@ class posts
     function displayFeed($contentItem)
     {
         
-        $feed = array();      
+        $feed = array();
         foreach ($contentItem as $node) {
             $item = array (
                 'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
@@ -54,87 +54,87 @@ class posts
         
     }
     
- 
+    
     /* This function will return all the items  from the XML .*/
     function displayhtmlfeed($feed)
     {
-        $max_item_cnt = count($feed);   
+        $max_item_cnt = count($feed);
         $result = "";
-       
+        
         for($x=0;$x<$max_item_cnt ;$x++)
         {
-           
+            
             /* create the div elemet for each feed but make sure to display the first feed visible*/
             if($x==0)
                 $result = '<div class="mysite"  id="feedid'. $x .'"><ul class="feed-lists">';
-            else
-                $result = '<div class="mysite" style="display:none" id="feedid'. $x .'"><ul class="feed-lists">';
+                else
+                    $result = '<div class="mysite" style="display:none" id="feedid'. $x .'"><ul class="feed-lists">';
                     
-           
-            
-            /*remove &amp; with &*/
-            $title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
-            
-            /*get the link url*/
-            $link = $feed[$x]['link'];
-            
-            /* add the heading with a link url in result */
-            $result .= '<div id="tit"><strong><a href="'.$link.'" target="_blank"  title="'.$title.'">'.$title.'</a></strong></div>';
-            
-            /* we will get the date format now*/
-            $date = date('l F d, Y', strtotime($feed[$x]['date']));
-            $result .= '<em>Posted on '.$date.'</em>';     
-            
-            /* place the description now */
-            $description = $feed[$x]['desc'];
-            $content = $feed[$x]['content'];
-            
-            // find the img
-            $has_image = preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $content, $image);
-            
-            // no html tags should be displayed
-            $description = strip_tags(preg_replace('/(<(script|style)\b[^>]*>).*?(<\/\2>)/s', "$1$3", $description), '');
-            
-            //remove ... from the description
-            $description = substr($description, 0, strpos($description, '...'));
-            
-            // add img if it exists
-            if ($has_image == 1) {
-                $result .= '<br><img class="feed-item-image" src="' . $image['src'] . '" /><br>';
-            }
-            
-            /* append description*/
-            
-            $result .= '<div class="feed-description">' . $description;
-            $result .= ' <a href="'.$link.'"  target="_blank" title="'.$title.'">Continue Reading..</a>'.'</div>';
-            
-            $result .= '<br></br>';
-        
-            $result .= '</ul></div>';     
-            echo $result;
+                    
+                    
+                    /*remove &amp; with &*/
+                    $title = str_replace(' & ', ' &amp; ', $feed[$x]['title']);
+                    
+                    /*get the link url*/
+                    $link = $feed[$x]['link'];
+                    
+                    /* add the heading with a link url in result */
+                    $result .= '<div id="tit"><strong><a href="'.$link.'" target="_blank"  title="'.$title.'">'.$title.'</a></strong></div>';
+                    
+                    /* we will get the date format now*/
+                    $date = date('l F d, Y', strtotime($feed[$x]['date']));
+                    $result .= '<em>Posted on '.$date.'</em>';
+                    
+                    /* place the description now */
+                    $description = $feed[$x]['desc'];
+                    $content = $feed[$x]['content'];
+                    
+                    // find the img
+                    $has_image = preg_match('/<img.+src=[\'"](?P<src>.+?)[\'"].*>/i', $content, $image);
+                    
+                    // no html tags should be displayed
+                    $description = filter_var($description, FILTER_SANITIZE_STRING);
+                    
+                    //remove ... from the description
+                    $description = substr($description, 0, strpos($description, '...'));
+                    
+                    // add img if it exists
+                    if ($has_image == 1) {
+                        $result .= '<br><img class="feed-item-image" src="' . $image['src'] . '" /><br>';
+                    }
+                    
+                    /* append description*/
+                    
+                    $result .= '<div class="feed-description">' . $description;
+                    $result .= ' <a href="'.$link.'"  target="_blank" title="'.$title.'">Continue Reading..</a>'.'</div>';
+                    
+                    $result .= '<br></br>';
+                    
+                    $result .= '</ul></div>';
+                    echo $result;
         }
         
         $this->displayhtmlfooter($max_item_cnt);
         
         
-     }
-     
-     function displayhtmlfooter($max)
-     {
-         
+    }
+    
+    function displayhtmlfooter($max)
+    {
+        
         /*  use below code to create link for each feed on one page so that user can jump to any feed directly.
          * uncomment the below uncomment code to use this code.
          for ($vx=0 ;$vx < $max; $vx++)
          {
-             $xvar= $vx+1;
-             echo '<a href="posts.php?id=' . $vx . '" class="btn">' . $xvar . ' </a>';
+         $xvar= $vx+1;
+         echo '<a href="posts.php?id=' . $vx . '" class="btn">' . $xvar . ' </a>';
          }*/
-         
-         /* more link to dislay feed one by one. code to display lies in javascript(myscript.js) file */
-         echo '<a href="javascript:void(0)" class ="btn" id="moreid"">..More Feed..</a>';
+        
+        /* more link to dislay feed one by one. code to display lies in javascript(myscript.js) file */
+        echo '<a href="javascript:void(0)" class ="btn" id="moreid"">..More Feed..</a>';
+        
+    }
     
-     }
-     
     
     
     
